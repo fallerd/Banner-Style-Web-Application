1c8b2a6eecf8cc77808fe80be26ff2b2a5c61cd8
d <fallerd@gmail.com>
2017-10-21 16:56:58 -0600

    comitting vaildation

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -1,4 +1,7 @@
 class Enrollment < ApplicationRecord
   belongs_to :section
   belongs_to :student
+
+  validates :student, uniqueness: { scope: :section, message: "already registered in that section" }
+
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,10 @@
 class Student < ApplicationRecord
 	has_many :enrollments
 	has_many :sections, through: :enrollments
+
+	validates :student_number, :email_address, uniqueness: true
+	validates :student_number, :email_address, :name, presence: true
+	validates :email_address, format: { with: /\A([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})\Z/, message: "Must be formatted as xxx@xxx.xxx"}
+	validates :student_number, format: { with: /900[0-9]{6}/, message: "Student number must have 9 digits and start with 900"}
+
 end
